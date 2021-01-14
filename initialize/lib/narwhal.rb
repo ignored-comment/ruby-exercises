3d14f0b332356f89edd5db5bcfc92970b266628e
ignored-comment <info.konham@gmail.com>
2021-01-13 23:27:06 -0500

    Complete narwhal exercise

---
+++ initialize/lib/narwhal.rb
@@ -0,0 +1,14 @@
+class Narwhal
+  attr_reader :name, :weight
+
+  def initialize(cute:, weight:, name:)
+    @cute = cute
+    @weight = weight
+    @name = name
+  end
+
+  def cute?
+    @cute
+  end
+
+end
