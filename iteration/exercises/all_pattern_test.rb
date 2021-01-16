da1f38bf39ea0ec94e6bf71bb7130d305bb72884
ignored-comment <info.konham@gmail.com>
2021-01-15 21:44:30 -0500

    Complete all_pattern exercise

--- iteration/exercises/all_pattern_test.rb
+++ iteration/exercises/all_pattern_test.rb
@@ -28,17 +28,20 @@ class AllPatternTest < Minitest::Test
   end
 
   def test_3
-    skip
+    # skip
     words = ["love", "hate", "fire", "bird", "call"]
     all_four_letters = true
     words.each do |word|
       # Your Code Here
+      if !word.length == 4
+        all_four_letters = false
+      end
     end
     assert_equal true, all_four_letters
   end
 
   def test_4
-    skip
+    # skip
     words = {
       one: "love",
       two: "hate",
@@ -49,21 +52,27 @@ class AllPatternTest < Minitest::Test
     all_four_letters = true
     words.each do |position, word|
       # Your Code Here
+      all_four_letters = false unless word.length == 4
     end
     assert_equal true, all_four_letters
   end
 
   def test_5
-    skip
+    # skip
     statuses = [:busy, :busy, :busy]
     all_busy = true
     # Your Code Here
+    statuses.each do |status|
+    if !status == :busy
+      all_busy = false
+    end
+  end
 
     assert_equal true, all_busy
   end
 
   def test_6
-    skip
+    # skip
     friend_status = {
       "Megan" => :busy,
       "Sarah" => :busy,
@@ -71,20 +80,27 @@ class AllPatternTest < Minitest::Test
     }
     all_busy = true
     # Your Code Here
+    friend_status.each do |position, word|
+      all_busy = false unless word == :busy
+    end
 
     assert_equal true, all_busy
   end
 
   def test_7
-    skip
+    # skip
+    all_five_digits = true
     zip_codes = [94381, 831, 50009, 36232, 8992, 89999, 11110]
     # Your Code Here
+    zip_codes.each do |code|
+      all_five_digits = false unless code.digits.count == 5
+      end
 
     assert_equal false, all_five_digits
   end
 
   def test_8
-    skip
+    # skip
     zip_codes = {
       "Megan" => 94381,
       "Sarah" => 831,
@@ -94,28 +110,37 @@ class AllPatternTest < Minitest::Test
       "Cameron" => 89999,
       "Joshua" => 11110
     }
-    # Your Code Here
+    all_five_digits = true
+    zip_codes.each do |key, value|
+      all_five_digits = false unless value.digits.count == 5
+    end
 
     assert_equal false, all_five_digits
   end
 
   def test_9
-    skip
+    # skip
     snacks = ["GARLIC PLANTAINS", "SNICKERDOODLES", "Pretzels"]
     # Your Code Here
-
+    all_caps = true
+    snacks.each do |snack|
+      all_caps = false unless snack.upcase == true
+    end
     assert_equal false, all_caps
   end
 
   def test_10
-    skip
+    # skip
     snacks = {
       savory: "GARLIC PLANTAINS",
       sweet: "SNICKERDOODLES",
       salty: "Pretzels"
     }
     # Your Code Here
-
+    all_caps = true
+    snacks.each do |key, value|
+      all_caps = false unless value.upcase == true
+    end
     assert_equal false, all_caps
   end
 end
