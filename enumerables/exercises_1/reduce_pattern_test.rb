7e3b4f5dd2ae2cc63a03ea92ac8c415f4c3ff544
ignored-comment <info.konham@gmail.com>
2021-01-22 00:57:03 -0500

    Complete reduce exercise

--- enumerables/exercises_1/reduce_pattern_test.rb
+++ enumerables/exercises_1/reduce_pattern_test.rb
@@ -8,7 +8,8 @@ class ReducePatternTest < Minitest::Test
     numbers = [32, 1, 21, 5, 81, 333]
     sum = 0
     numbers.each do |number|
-      sum = sum + number
+      # sum = sum + number
+      sum += number
     end
     assert_equal 473, sum
   end
@@ -18,47 +19,64 @@ class ReducePatternTest < Minitest::Test
     difference = 0
     numbers.each do |number|
       # Your code goes here
+      difference -= number
     end
     assert_equal -170, difference
   end
 
   def test_multiply_list_of_numbers
-    skip
+    # skip
     numbers = [2, 3, 5, 7]
     product = 1
     # Your code goes here
+    numbers.each do |number|
+      product *= number
+    end
     assert_equal 210, product
   end
 
   def test_capitalize_keywords_in_phrase_one_fish_two_fish_red_fish_blue_fish
-    skip
+    # skip
     keywords = ["fish", "blue"]
     phrase = 'one fish two fish red fish blue fish'
     # Your code goes here
+    keywords.each do |word|
+      phrase = phrase.gsub(word, word.upcase)
+    end
     assert_equal 'one FISH two FISH red FISH BLUE FISH', phrase
   end
 
   def test_divide_560_by_a_bunch_of_numbers
-    skip
+    # skip
     numbers = [2, 2, 2, 5, 7]
     quotient = 560
+
+    numbers.each do |number|
+      quotient /= number
+    end
     # Your code goes here
     assert_equal 2, quotient
   end
 
   def test_subtract_smallest_values_from_100
-    skip
+    # skip
     elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
     difference = 100
     # Your code goes here
+    elements.each do |element|
+      difference -= element.min
+    end
     assert_equal 88, difference
   end
 
   def test_add_all_the_second_values_together
-    skip
+    # skip
     elements = [["a", 1], ["b", 9], ["c", 21]]
     sum = 0
     # Your code goes here
+    elements.each do |element|
+      sum += element[1]
+    end
     assert_equal 31, sum
   end
 
